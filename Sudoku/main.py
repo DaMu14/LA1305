863eb9b3ec68fd065bbb8c7749b0f06feb712f59
DaMu14 <da407mu@gmail.com>
2024-05-17 11:46:23 +0200

    Update main.py
    asd

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -3,4 +3,11 @@ from tkinter import messagebox
 import random
 import copy
 
- 
+def create_sudoku_field():
+ i=0
+ for y in range (0, 900, 100):
+  for x in range (0, 900, 100):
+   fields[i] = [pygame.Rect(x, y, 100, 100), sudoku[i]]
+   pygame.draw.rect(screen, (0, 0, 0), fields[i][0], 1)
+
+   
